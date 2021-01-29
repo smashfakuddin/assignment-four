36c637b596e02b2686a92b7a7a3f82cfa7000b04
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-01-29 23:30:02 +0600

    commit

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-document.getElementById('increase').addEventListener('click', function(){
+document.getElementById('increase').addEventListener('click', function () {
+    testFunction(true);
+    
+
+})
+document.getElementById('decrease').addEventListener('click', function () {
+    testFunction(false);
+    
+    
+})
+
+
+function testFunction(wantIncrease) {
     var secondClassSeatCount = document.getElementById("second-class");
     var secondClassSeatCountNumber = parseInt(secondClassSeatCount.value);
-    var secondClassSeatCountNew = secondClassSeatCountNumber + 1;
+    var secondClassSeatCountNew = 0;
+    if (wantIncrease == true) {
+        var secondClassSeatCountNew = secondClassSeatCountNumber + 1;
+    }
+    if (wantIncrease == false && secondClassSeatCountNumber > 0) {
+        var secondClassSeatCountNew = secondClassSeatCountNumber - 1;
+    }
     secondClassSeatCount.value = secondClassSeatCountNew;
-})
+
+    caseTotal = secondClassSeatCountNew * 100 ;
+    document.getElementById('secondClass-price').innerText = caseTotal;
+    
+}
+
+
+
+
+
