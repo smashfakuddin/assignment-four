f94410884bf8fe1778a25dfd537b031571a12e32
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-01-29 23:46:13 +0600

    commit

--- main.js
+++ main.js
@@ -1,33 +1,52 @@
-document.getElementById('increase').addEventListener('click', function () {
+document.getElementById('economy-increase').addEventListener('click', function () {
     testFunction(true);
-    
 
 })
-document.getElementById('decrease').addEventListener('click', function () {
+document.getElementById('economy-decrease').addEventListener('click', function () {
     testFunction(false);
-    
-    
+
 })
 
+document.getElementById('firstClass-increase').addEventListener('click', function () {
+    testFunction2(true);
 
-function testFunction(wantIncrease) {
-    var secondClassSeatCount = document.getElementById("second-class");
+
+})
+document.getElementById('firstClass-decrease').addEventListener('click', function () {
+    testFunction2(false);
+
+})
+
+function testFunction(wantToIncrease) {
+    var secondClassSeatCount = document.getElementById("economy-class");
     var secondClassSeatCountNumber = parseInt(secondClassSeatCount.value);
     var secondClassSeatCountNew = 0;
-    if (wantIncrease == true) {
+    if (wantToIncrease == true) {
         var secondClassSeatCountNew = secondClassSeatCountNumber + 1;
     }
-    if (wantIncrease == false && secondClassSeatCountNumber > 0) {
+    if (wantToIncrease == false && secondClassSeatCountNumber > 0) {
         var secondClassSeatCountNew = secondClassSeatCountNumber - 1;
     }
     secondClassSeatCount.value = secondClassSeatCountNew;
 
-    caseTotal = secondClassSeatCountNew * 100 ;
-    document.getElementById('secondClass-price').innerText = caseTotal;
-    
+    secondClassTotal = secondClassSeatCountNew * 100;
+    document.getElementById('economyClass-price').innerText = secondClassTotal;
+
 }
 
+function testFunction2(wantToIncrease) {
+    var secondClassSeatCount = document.getElementById("first-class");
+    var secondClassSeatCountNumber = parseInt(secondClassSeatCount.value);
+    var secondClassSeatCountNew = 0;
+    if (wantToIncrease == true) {
+        var secondClassSeatCountNew = secondClassSeatCountNumber + 1;
+    }
+    if (wantToIncrease == false && secondClassSeatCountNumber > 0) {
+        var secondClassSeatCountNew = secondClassSeatCountNumber - 1;
+    }
+    secondClassSeatCount.value = secondClassSeatCountNew;
 
+    secondClassTotal = secondClassSeatCountNew * 150;
+    document.getElementById('firstClass-price').innerText = secondClassTotal;
 
-
-
+}
